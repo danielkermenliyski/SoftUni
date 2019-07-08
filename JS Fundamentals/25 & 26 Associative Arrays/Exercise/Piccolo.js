6a03b20bd00728437bf9f273cd24f97e17a4d681
Daniel Kermenliyski <daniel.kermenliyski@gfk.com>
2019-07-08 09:41:18 +0300

    Add new files

---
+++ JS Fundamentals/25 & 26 Associative Arrays/Exercise/Piccolo.js
@@ -0,0 +1,35 @@
+function solve(input) {
+
+    let cars = {};
+
+    for (const elem of input) {
+        let [command, number] = elem.split(', ');
+        if (command === 'IN') {
+            cars[number] = 0;
+        } else {
+            if(cars.hasOwnProperty(number)) {
+                delete cars[number];
+            }
+        }
+    }
+
+    let sorted = [...Object.entries(cars)];
+    sorted.sort((a,b) =>
+        a[0].localeCompare(b[0]));
+
+    for (const car of sorted) {
+        console.log(car[0]);
+
+    }
+}
+
+solve(['IN, CA2844AA',
+    'IN, CA1234TA',
+    'OUT, CA2844AA',
+    'IN, CA9999TT',
+    'IN, CA2866HI',
+    'OUT, CA1234TA',
+    'IN, CA2844AA',
+    'OUT, CA2866HI',
+    'IN, CA9876HH',
+    'IN, CA2822UU']);
