861b31af507559b5a65cfc8c3d3900ce6e2148e2
daniel.kermenliyski <dstoianov891@gmail.com>
2019-07-20 11:20:49 +0300

    Add new files

--- JS Fundamentals/31 & 32 Regular Expressions/Lab/03_Match_Dates.js
+++ JS Fundamentals/31 & 32 Regular Expressions/Lab/03_Match_Dates.js
@@ -10,9 +10,6 @@ function solve(dates) {
         console.log(`Day: ${day}, Month: ${month}, Year: ${year}`);
     }
 
-    console.log(validPhones.join(', '));
-
-
 }
 
 solve("13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973, 1/Feb/2016");
