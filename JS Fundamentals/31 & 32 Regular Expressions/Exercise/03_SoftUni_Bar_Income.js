7852034fdf02b33b7abfc7659924905059f64f47
Daniel Kermenliyski <daniel.kermenliyski@gfk.com>
2019-07-22 12:07:28 +0300

    Add new files

--- JS Fundamentals/31 & 32 Regular Expressions/Exercise/03_SoftUni_Bar_Income.js
+++ JS Fundamentals/31 & 32 Regular Expressions/Exercise/03_SoftUni_Bar_Income.js
@@ -6,10 +6,13 @@ function solve(arr) {
         let pattern = /%([A-Z][a-z]+)%[^|$%.]*<(\w+)>[^|$%.]*\|([0-9]+)\|[^|$%.]*?([0-9]+\.?[0-9]*)\$/gm;
 
         let result = pattern.exec(line);
-        let currentSum = Number(result[3]) * Number(result[4]);
-        console.log(`${result[1]}: ${result[2]} - ${currentSum.toFixed(2)}`);
-        totalIncome += currentSum;
-    });
+        if (result) {
+            let currentSum = Number(result[3]) * Number(result[4]);
+            console.log(`${result[1]}: ${result[2]} - ${currentSum.toFixed(2)}`);
+            totalIncome += currentSum;
+        }
+        });
+
     console.log(`Total income: ${totalIncome.toFixed(2)}`);
 
 }
@@ -20,8 +23,8 @@ solve(['%George%<Croissant>|2|10.3$',
     'end of shift']
 );
 
-// solve([ '%InvalidName%<Croissant>|2|10.3$',
-//     '%Peter%<Gum>1.3$',
-//     '%Maria%<Cola>|1|2.4',
-//     '%Valid%<Valid>valid|10|valid20$',
-//     'end of shift' ]);
+solve([ '%InvalidName%<Croissant>|2|10.3$',
+    '%Peter%<Gum>1.3$',
+    '%Maria%<Cola>|1|2.4',
+    '%Valid%<Valid>valid|10|valid20$',
+    'end of shift' ]);
