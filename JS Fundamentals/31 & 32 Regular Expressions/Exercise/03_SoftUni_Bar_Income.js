f3ab5276585aec0c5c301a133b94cb921d145bd2
Daniel Kermenliyski <daniel.kermenliyski@gfk.com>
2019-07-22 09:56:46 +0300

    Add new files

---
+++ JS Fundamentals/31 & 32 Regular Expressions/Exercise/03_SoftUni_Bar_Income.js
@@ -0,0 +1,27 @@
+function solve(arr) {
+
+    arr.pop();
+    let totalIncome = 0;
+    arr.forEach((line) => {
+        let pattern = /%([A-Z][a-z]+)%<(\w+)>\|([0-9]+)\|([0-9]+\.?[0-9]*)\$/gm;
+
+        let result = pattern.exec(line);
+        let currentSum = Number(result[3]) * Number(result[4]);
+        console.log(`${result[1]}: ${result[2]} - ${currentSum.toFixed(2)}`);
+        totalIncome += currentSum;
+    });
+    console.log(`Total income: ${totalIncome.toFixed(2)}`);
+
+}
+
+solve(['%George%<Croissant>|2|10.3$',
+    '%Peter%<Gum>|1|1.3$',
+    '%Maria%<Cola>|1|2.4$',
+    'end of shift']
+);
+
+//solve([ '%InvalidName%<Croissant>|2|10.3$',
+//     '%Peter%<Gum>1.3$',
+//     '%Maria%<Cola>|1|2.4',
+//     '%Valid%<Valid>valid|10|valid20$',
+//     'end of shift' ]);
